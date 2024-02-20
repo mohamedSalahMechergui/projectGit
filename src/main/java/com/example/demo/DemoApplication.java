c111426b587d0de3d4f0a17eb2b395b989a21db2
mohamedsalahmechergui <mohamed.salah.mechergui@gmail.com>
2024-02-20 19:10:46 +0100

    med commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	public static int factorielle(int n) {
+		int resultat = 1;
+		for (int i = 1; i <= n; i++) {
+			resultat *= i;
+		}
+		return resultat;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
